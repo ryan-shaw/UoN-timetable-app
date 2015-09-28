8eed0def4301f8ecdcc11d5e16e33e194a1d9819
Ryan Shaw <ryan.shaw@min.vc>
2015-09-28 20:34:34 +0100

    Added build file and assets. Change package name.

---
+++ make.sh
@@ -0,0 +1,4 @@
+rm Timetable.apk
+ionic build --release android
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release-key.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk timetable
+$ANDROID_HOME/build-tools/22.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk Timetable.apk
